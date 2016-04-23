ec1ee4b63b173302729fa3d4ac7135b2ac143349
Martin Prudek <prudemar@fel.cvut.cz>
2016-04-24 01:18:20 +0200

    sssss

--- httpd.c
+++ httpd.c
@@ -136,7 +136,7 @@ void accept_request(int client){
 		recv(client,data_buf,length,0);
 		len=*((int *)&(data_buf[length-4]));
 		printf("delka decompressed=%d\n",len);
-		if (len==0) len=10*length;
+		if (len==0) len=2*length;
 		decomp = calloc(len,sizeof(char));
 		len2=len;
 		while(1){
